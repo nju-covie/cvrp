2fdd1a6b11c030a6d3fb500caab1fcbc04cc7ae2
covie <icovie@163.com>
2021-12-08 21:31:42 +0800

    try with resources

--- src/main/java/io/InstanceLoader.java
+++ src/main/java/io/InstanceLoader.java
@@ -15,33 +15,33 @@ public class InstanceLoader {
 
     public static Instance load(String path) throws FileNotFoundException {
         Instance instance = new Instance();
-        Scanner cin = new Scanner(new FileReader(path));
-        for (int i = 0; i < 4; i++) {
-            cin.nextLine();
+        try (Scanner cin = new Scanner(new FileReader(path))) {
+            for (int i = 0; i < 4; i++) {
+                cin.nextLine();
+            }
+            String line = cin.nextLine();
+            String[] lineSplit = line.trim().split("\\s+");
+            instance.setVehicleNum(Integer.parseInt(lineSplit[0]));
+            instance.setVehicleCapacity(Integer.parseInt(lineSplit[1]));
+            for (int i = 0; i < 4; i++) {
+                cin.nextLine();
+            }
+            List<Integer> x = new ArrayList<>();
+            List<Integer> y = new ArrayList<>();
+            List<Integer> demand = new ArrayList<>();
+            while (cin.hasNext()) {
+                line = cin.nextLine();
+                lineSplit = line.trim().split("\\s+");
+                x.add(Integer.parseInt(lineSplit[1]));
+                y.add(Integer.parseInt(lineSplit[2]));
+                demand.add(Integer.parseInt(lineSplit[3]));
+            }
+            instance.setX(x);
+            instance.setY(y);
+            instance.setDemand(demand);
+            instance.setNodeNum(x.size());
+            instance.calDistance();
         }
-        String line = cin.nextLine();
-        String[] lineSplit = line.trim().split("\\s+");
-        instance.setVehicleNum(Integer.parseInt(lineSplit[0]));
-        instance.setVehicleCapacity(Integer.parseInt(lineSplit[1]));
-        for (int i = 0; i < 4; i++) {
-            cin.nextLine();
-        }
-
-        List<Integer> x = new ArrayList<>();
-        List<Integer> y = new ArrayList<>();
-        List<Integer> demand = new ArrayList<>();
-        while (cin.hasNext()) {
-            line = cin.nextLine();
-            lineSplit = line.trim().split("\\s+");
-            x.add(Integer.parseInt(lineSplit[1]));
-            y.add(Integer.parseInt(lineSplit[2]));
-            demand.add(Integer.parseInt(lineSplit[3]));
-        }
-        instance.setX(x);
-        instance.setY(y);
-        instance.setDemand(demand);
-        instance.setNodeNum(x.size());
-        instance.calDistance();
         return instance;
     }
 }
