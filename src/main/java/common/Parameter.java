7d32ebafebf04b8fb06969b63a2399e8888f6577
covie <icovie@163.com>
2021-12-08 21:32:00 +0800

    modify parameter

--- src/main/java/common/Parameter.java
+++ src/main/java/common/Parameter.java
@@ -1,7 +1,7 @@
 package common;
 
 public class Parameter {
-    public static final int TABU_TENURE = 8;
+    public static final int TABU_TENURE = 10;
     public static final int MAX_ITER = 10000;
     public static final int MAX_NON_IMPROVE = 200;
     public static final int SHAKE_TENURE = 4;
